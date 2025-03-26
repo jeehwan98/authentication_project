15437ac88184d6f1c2a09c909af1ba12e4c24742
Jee Hwan <jeehwan98@naver.com>
2025-03-27 04:25:11 +0900

    fix: github button

--- front/src/components/auth/form/github-button.tsx
+++ front/src/components/auth/form/github-button.tsx
@@ -1,15 +1,30 @@
 "use client";
 
 import { Button } from "@/components/ui/button"
+import { useTheme } from "next-themes";
+import Image from "next/image";
 import React from "react";
 
 export default function GithubSignInButton() {
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const { theme } = useTheme();
+  const logo = theme === "dark" ? "/images/githubDarkLogo.png" : "/images/githubLightLogo.png";
+
+  const handleGithubLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    window.location.href = "http://localhost:8080/api/v1/auth/login/github";
   };
 
   return (
-    <Button onClick={handleClick} className="w-full">
+    <Button
+      onClick={handleGithubLogin}
+      className="w-full mt-5"
+    >
+      <Image
+        src={logo}
+        alt="Github Logo"
+        width={20}
+        height={20}
+      />
       Sign In with Github
     </Button>
   )
