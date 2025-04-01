b2ddb168b439ecd6beef6b6b917fb4f235e3a7c2
Jee Hwan <jeehwan98@naver.com>
2025-04-01 18:23:30 +0900

    fix: server session

--- front/src/app/profile/[userId]/page.tsx
+++ front/src/app/profile/[userId]/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "@/lib/auth/auth-server";
 
-export default function UserProfile() {
-  const { user } = getServerSession();
+export default async function UserProfile() {
+  const user = await getServerSession();
 
   if (!user) {
     return <div>User not found</div>
